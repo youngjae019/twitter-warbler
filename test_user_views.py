5c49135b68c4a63be8e42a7d4a68a9d7d63b1f47
Katherine Godwin <kgodwin019@gmail.com>
2020-08-18 20:41:35 -0400

    added tests

---
+++ test_user_views.py
@@ -0,0 +1,75 @@
+import os
+from unittest import TestCase
+
+from models import db, connect_db, Message, User, Likes, Follows
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+
+from app import app, CURR_USER_KEY
+
+db.create_all()
+
+app.config['WTF_CSRF_ENABLED'] = False
+
+class MessageViewTestCase(TestCase):
+    """Test views for messages."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+
+        db.drop_all()
+        db.create_all()
+
+        self.client = app.test_client()
+
+        self.testuser = User.signup(username="testuser",
+                                    email="test@test.com",
+                                    password="testuser",
+                                    image_url=None)
+        self.testuser_id = 8989
+        self.testuser.id = self.testuser_id
+
+        self.u1 = User.signup("abc", "test1@test.com", "password", None)
+        self.u1_id = 778
+        self.u1.id = self.u1_id
+        self.u2 = User.signup("efg", "test2@test.com", "password", None)
+        self.u2_id = 884
+        self.u2.id = self.u2_id
+        self.u3 = User.signup("hij", "test3@test.com", "password", None)
+        self.u4 = User.signup("testing", "test4@test.com", "password", None)
+
+        db.session.commit()
+
+    def tearDown(self):
+        resp = super().tearDown()
+        db.session.rollback()
+        return resp
+
+    def test_users_index(self):
+        with self.client as c:
+            resp = c.get("/users")
+
+            self.assertIn("@testuser", str(resp.data))
+            self.assertIn("@abc", str(resp.data))
+            self.assertIn("@efg", str(resp.data))
+            self.assertIn("@hij", str(resp.data))
+            self.assertIn("@testing", str(resp.data))
+
+    def test_users_search(self):
+        with self.client as c:
+            resp = c.get("/users?q=test")
+
+            self.assertIn("@testuser", str(resp.data))
+            self.assertIn("@testing", str(resp.data))            
+
+            self.assertNotIn("@abc", str(resp.data))
+            self.assertNotIn("@efg", str(resp.data))
+            self.assertNotIn("@hij", str(resp.data))
+
+    def test_user_show(self):
+        with self.client as c:
+            resp = c.get(f"/users/{self.testuser_id}")
+
+            self.assertEqual(resp.status_code, 200)
+
+            self.assertIn("@testuser", str(resp.data))
